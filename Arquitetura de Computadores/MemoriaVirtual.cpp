ca688b120e7434b6b6f0bc4275cc9e99059e2db8
Mateus de Oliveira Lopes <lopes3137@gmail.com>
2024-02-16 08:57:21 -0300

    Trabalho sobre memoria virtual em c++

--- Arquitetura de Computadores/MemoriaVirtual.cpp
+++ Arquitetura de Computadores/MemoriaVirtual.cpp
@@ -11,7 +11,7 @@ struct Page{
 void criarPaginas(Page*, int*);
 void criarRAM(int*, int, int*);
 void criarDisco(int*, int, int*);
-int buscarMV(int,Page*,int*,int*,int,int*,int*);
+int buscarMV(int,Page*,int*,int*,int,int*,int*,int*);
 void resultadosBMV(int,int,int*,int*);
 
 int main()
@@ -33,27 +33,37 @@ int main()
     criarRAM(memoriaRAM,tamanhoMemoriaRAM,&RAMUsada);
     criarDisco(memoriaDisco,tamanhoMemoriaDisco,&discoUsado);
 
+    cout << "Iniciando testes de busca de endereco virtual: \n" << endl;
+    
     endereco = 42;
-    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada);
+    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada,&discoUsado);
     resultadosBMV(resultadoBusca, endereco, &pageMiss, &pageHit);
     
     endereco = 75;
-    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada);
+    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada,&discoUsado);
     resultadosBMV(resultadoBusca, endereco, &pageMiss, &pageHit);
     
     endereco = 17;
-    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada);
+    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada,&discoUsado);
     resultadosBMV(resultadoBusca, endereco, &pageMiss, &pageHit);
     
     endereco = 12;
-    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada);
+    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada,&discoUsado);
     resultadosBMV(resultadoBusca, endereco, &pageMiss, &pageHit);
 
     endereco = 13;
-    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada);
+    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada,&discoUsado);
     resultadosBMV(resultadoBusca, endereco, &pageMiss, &pageHit);
 
-    cout << "Page Hit(s): " << pageHit << endl << "Page Miss: " << pageMiss << endl; 
+    endereco = 10;
+    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada,&discoUsado);
+    resultadosBMV(resultadoBusca, endereco, &pageMiss, &pageHit);
+
+    endereco = 42;
+    resultadoBusca = buscarMV(endereco,pagina,memoriaRAM,memoriaDisco,tamanhoMemoriaRAM,&paginaUsada,&RAMUsada,&discoUsado);
+    resultadosBMV(resultadoBusca, endereco, &pageMiss, &pageHit);
+
+    cout << "Page Hit(s): " << pageHit << endl << "Page Miss(es): " << pageMiss << endl; 
 }
 
 void criarPaginas(Page *p, int *n)
@@ -77,6 +87,12 @@ void criarPaginas(Page *p, int *n)
     (p+3)->enderecoFisico = 8;
     (p+3)->isDisco = true;
     *n = *n + 1;
+
+    (p+4)->enderecoVirtual = 10;
+    (p+4)->enderecoFisico = 3;
+    (p+4)->isDisco = false;
+    *n = *n + 1;
+
 }
 
 void criarRAM(int *p, int n, int *m)
@@ -97,7 +113,7 @@ void criarDisco(int *p, int n, int *m)
     }
 }
 
-int buscarMV(int end,Page *p, int *r, int *d, int tR, int *pU, int *rU)
+int buscarMV(int end,Page *p, int *r, int *d, int tR, int *pU, int *rU, int *dU)
 {
     for(int i = 0; i < *pU; i++)                        //Para todos os valores de paginas usados
     {
@@ -124,9 +140,10 @@ int buscarMV(int end,Page *p, int *r, int *d, int tR, int *pU, int *rU)
                 {
                     cout << "RAM nao tem espaco. Substituindo um valor na RAM pelo valor no disco" << endl;
                     //Aqui seria usada uma política de substituição, cujo qual demoraria muito para programar
-                    //Portanto, vou só substituir no primeiro valor do vetor
+                    //Portanto, vou só substituir no primeiro endereço do vetor
                     r[0] = d[(p+i)->enderecoFisico];    //transferencia de disco pra RAM
                     d[(p+i)->enderecoFisico] = 0;       //Anula o valor em disco pós transferência (melhor maneira que consegui representar)
+                    *dU = *dU - 1;
                     (p+i)->enderecoFisico = 0;          //Atualiza a pagina
                     return r[(p+i)->enderecoFisico];    //Retorna o valor salvo no endereço em RAM
                 }
@@ -142,11 +159,12 @@ void resultadosBMV(int rB, int end, int *pM, int *pH)
     {
         *pM = *pM + 1;
         cout << "O valor do endereco virtual " << end << " nao esta na pagina (Page Miss)" << endl;
+        cout << "\n";
     }
     else
     {
         *pH = *pH + 1;
         cout << "o valor no endereco virtual "<< end << " eh: " << rB << " (Page Hit)" << endl;
         cout << "\n";
-    }
+     }
 }
