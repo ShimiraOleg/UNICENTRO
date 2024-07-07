e09621f4566041ca313a525f31abab60c66a1c06
Yan Gabriel <yangabrielreis@gmail.com>
2024-07-07 14:38:30 -0300

    atualizado classe console pra printar tabuleiro

--- POOI/JogoDaVelha/src/entradaDados/Console.java
+++ POOI/JogoDaVelha/src/entradaDados/Console.java
@@ -1,4 +1,19 @@
 package entradaDados;
+import java.util.Scanner;
 
 public class Console {
+  public static void printTabuleiro(String[] tabuleiro) {
+    System.out.println(" " + tabuleiro[0] + " | " + tabuleiro[1] + " | " + tabuleiro[2]);
+    System.out.println("---+---+---");
+    System.out.println(" " + tabuleiro[3] + " | " + tabuleiro[4] + " | " + tabuleiro[5]);
+    System.out.println("---+---+---");
+    System.out.println(" " + tabuleiro[6] + " | " + tabuleiro[7] + " | " + tabuleiro[8]);
+  }
+
+  public static void jogada(){  
+    System.out.println("Digite a posição que deseja jogar: ");
+    Scanner scanner = new Scanner(System.in);
+    int posicao = scanner.nextInt();
+
+  }
 }
