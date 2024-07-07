9ce3d2cc7a4da95d5aad5c7383cde716fde2c7d3
ShimiraOleg <lopes3137@gmail.com>
2024-07-07 16:22:23 -0300

    implementacao do console no Main

--- POOI/JogoDaVelha/src/jogoDaVelha/Main.java
+++ POOI/JogoDaVelha/src/jogoDaVelha/Main.java
@@ -1,14 +1,19 @@
 package jogoDaVelha;
 
+import entradaDados.Console;
+
+import java.lang.invoke.ConstantCallSite;
+
 public class Main {
     public static void main(String[] args) {
         char vencedor;
+        Console con = new Console();
         boolean isWin = false;
         Tabuleiro tab = new Tabuleiro();
-
         while(tab.getRodadas() < 9 && !(isWin))
         {
-            tab.setTabuleiro("O", 1);
+            con.printTabuleiro(tab.getTabuleiro());
+            tab.setTabuleiro("O", con.receberEntradaJogada());
             vencedor = tab.verificarSeTemVencedor(tab.getTabuleiro());
             isWin = tab.analizarVencedor(tab.getRodadas(), vencedor);
             tab.setRodadas();
