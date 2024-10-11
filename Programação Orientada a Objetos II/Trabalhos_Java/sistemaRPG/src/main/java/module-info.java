8f42b79dbc75afcc90325891714a9c5b8d13d4fe
ShimiraOleg <lopes3137@gmail.com>
2024-10-11 09:53:35 -0300

    Atividades Java

---
+++ Programação Orientada a Objetos II/Trabalhos_Java/sistemaRPG/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.trabalhojava.sistemarpg {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+}
