b18ce72a202d2c0891b505ee1c9f53e262907cc1
Mateus de Oliveira Lopes <Lopes3137@gmail.com>
2025-08-13 10:10:11 -0300

    Exercicios 13/08/25

--- Desenvolvimento Para Dispositivos Móveis/Dart/Exercicios_1/bin/ex_4.dart
+++ Desenvolvimento Para Dispositivos Móveis/Dart/Exercicios_1/bin/ex_4.dart
@@ -2,6 +2,7 @@ void main(){
   int A = 4;
   int B = 17;
   int C = 3;
-  List resultado = [A,B,C];
+  List<int> resultado = [A,B,C];
+  resultado.sort((D,E) => E.compareTo(D));
   print(resultado);
-}
+} 
