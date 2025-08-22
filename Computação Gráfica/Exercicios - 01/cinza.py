aa3a557a65f466082019145967693dd2a2a9108f
Mateus de Oliveira Lopes <Lopes3137@gmail.com>
2025-08-22 13:16:58 -0300

    Exercicio 1

--- Computação Gráfica/Exercicios - 01/cinza.py
+++ Computação Gráfica/Exercicios - 01/cinza.py
@@ -1,6 +1,38 @@
 import cv2
+import numpy as np
 
-img1 = cv2.imread('Imagens/lena.png', cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread('Imagens/img_aluno.png', cv2.IMREAD_GRAYSCALE)
-cv2.imwrite('Imagens/lena_cinza.png', img1)
-cv2.imwrite('Imagens/img_aluno_cinza.png', img2)
+def nivelDeCinza(img):
+    img = img.astype(np.float16)
+    c = img[:,:,0]/3 + img[:,:,1]/3 + img[:,:,2]/3
+    c = c.astype(np.uint8)
+    return c
+
+def negativo(img):
+    c = 255 - img
+    return c
+
+def normalizacao(img, c, d):
+    a = np.min(img)
+    b = np.max(img)
+    imagemFinal = (img-a)*((d-c)/(b-a))+c
+    imagemFinal = imagemFinal.astype(np.uint8)
+    return imagemFinal
+
+def operadorLog(img):
+    imagemFinal = img
+    return imagemFinal
+
+def mostrarImagem(img, mensagem):
+    cv2.imshow(mensagem, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+img1 = cv2.imread('Imagens/lena.png')
+cinza = nivelDeCinza(img1)
+negative = negativo(img1)
+normalizada = normalizacao(img1, 0, 100)
+mostrarImagem(cinza, "Imagem Normal")
+mostrarImagem(img1, "Realce de Cor 100%")
+mostrarImagem(negative, "Imagem Cinza")
+mostrarImagem(normalizada, "Imagem Cinza Sem Tratamento")
+#cv2.imwrite("Imagens/lena_cinza.png",output)
