8d3db05c94e01ecccf381d2bab47156a443a63fc
Mateus de Oliveira Lopes <Lopes3137@gmail.com>
2025-09-01 11:08:30 -0300

    IA II - Trabalho I

--- Computação Gráfica/Exercicios - 01/cinza.py
+++ Computação Gráfica/Exercicios - 01/cinza.py
@@ -19,8 +19,17 @@ def normalizacao(img, c, d):
     return imagemFinal
 
 def operadorLog(img):
-    imagemFinal = img
+    img = img.astype(np.float16)
+    c = 255/ np.log(1 + np.max(img))
+    imagemLog = c * (np.log(img + 1))
+    imagemFinal = imagemLog.astype(np.uint8)
     return imagemFinal
+
+def potencia(img, c, r):
+    img = img.astype(np.float32)
+    imgPow = c * np.power(img, r)
+    final = normalizacao(imgPow,0,255)
+    return final
 
 def mostrarImagem(img, mensagem):
     cv2.imshow(mensagem, img)
@@ -28,11 +37,31 @@ def mostrarImagem(img, mensagem):
     cv2.destroyAllWindows()
 
 img1 = cv2.imread('Imagens/lena.png')
+img2 = cv2.imread('Imagens/img_aluno.png')
 cinza = nivelDeCinza(img1)
 negative = negativo(img1)
 normalizada = normalizacao(img1, 0, 100)
-mostrarImagem(cinza, "Imagem Normal")
-mostrarImagem(img1, "Realce de Cor 100%")
-mostrarImagem(negative, "Imagem Cinza")
-mostrarImagem(normalizada, "Imagem Cinza Sem Tratamento")
-#cv2.imwrite("Imagens/lena_cinza.png",output)
+logaritmo = operadorLog(img1)
+power = potencia(img1, 2, 2)
+cinza2 = nivelDeCinza(img2)
+negative2 = negativo(img2)
+normalizada2 = normalizacao(img2, 0, 100)
+logaritmo2 = operadorLog(img2)
+power2 = potencia(img2, 2, 2)
+
+mostrarImagem(img1, "Imagem Original")
+mostrarImagem(cinza, "Imagem Cinza")
+mostrarImagem(negative, "Imagem Negativa")
+mostrarImagem(normalizada, "Imagem Normalizada")
+mostrarImagem(logaritmo, "Imagem Logaritmo")
+mostrarImagem(power, "Imagem Potencia")
+cv2.imwrite("Imagens/lena_cinza.png",cinza)
+cv2.imwrite("Imagens/lena_negativa.png",negative)
+cv2.imwrite("Imagens/lena_normalizada.png",normalizada)
+cv2.imwrite("Imagens/lena_log.png",logaritmo)
+cv2.imwrite("Imagens/lena_potencia.png",power)
+cv2.imwrite("Imagens/img_aluno_cinza.png",cinza2)
+cv2.imwrite("Imagens/img_aluno_negativa.png",negative2)
+cv2.imwrite("Imagens/img_aluno_normalizada.png",normalizada2)
+cv2.imwrite("Imagens/img_aluno_log.png",logaritmo2)
+cv2.imwrite("Imagens/img_aluno_potencia.png",power2)
